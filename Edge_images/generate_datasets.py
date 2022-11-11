d0478c4eb54894e62c2f5870d68f65b6af278223
jakob-bjorner <jakob_bjorner@hotmail.com>
2022-11-11 17:10:03 -0500

    generate dexi dataset

--- Edge_images/generate_datasets.py
+++ Edge_images/generate_datasets.py
@@ -33,22 +33,22 @@ class DexiNedDataset(Dataset):
 class DexiNedUnlabeledDataset(DexiNedDataset):
     # all labels are -1
     def __init__(self,
-                 csv_file="datasets/Edge_images/Dexi/unlabeled/labels.csv", 
-                 root_dir="datasets/Edge_images/Dexi/unlabeled",
+                 csv_file="Edge_images/Dexi/unlabeled/labels.csv", 
+                 root_dir="Edge_images/Dexi/unlabeled",
                  transform=None):
         super().__init__(csv_file, root_dir, transform) 
 
 class DexiNedTrainDataset(DexiNedDataset):
     def __init__(self, 
-                csv_file="datasets/Edge_images/Dexi/train/labels.csv", 
-                root_dir="datasets/Edge_images/Dexi/train", 
+                csv_file="Edge_images/Dexi/train/labels.csv", 
+                root_dir="Edge_images/Dexi/train", 
                 transform=None):
         super().__init__(csv_file, root_dir, transform)
 
 class DexiNedTestDataset(DexiNedDataset):
     def __init__(self,
-                 csv_file="datasets/Edge_images/Dexi/test/labels.csv",
-                 root_dir="datasets/Edge_images/Dexi/test",
+                 csv_file="Edge_images/Dexi/test/labels.csv",
+                 root_dir="Edge_images/Dexi/test",
                  transform=None):
         super().__init__(csv_file, root_dir, transform)
 
@@ -56,8 +56,9 @@ class DexiNedTestDataset(DexiNedDataset):
 def preprocess_all_stl10(stl_data_path, destination_path_root, transform=DexinedAug()):
     """Convert all images from stl10 to edge versions and save them in the same directory"""
     print("provided stl_data_path", stl_data_path)
+    from torchvision import transforms 
     for ds_split in ['train', 'unlabeled', 'test']:
-        ds = datasets.STL10(stl_data_path, split=ds_split, transform=transform, download=True)
+        ds = datasets.STL10(stl_data_path, split=ds_split, transform=transforms.ToTensor(), download=True)
         loader = torch.utils.data.DataLoader(
             ds,
             num_workers=1,
@@ -68,14 +69,17 @@ def preprocess_all_stl10(stl_data_path, destination_path_root, transform=Dexined
         split_dir = os.path.join(destination_path_root, ds_split)
         os.makedirs(split_dir, exist_ok=True)
         for i, (image, label) in enumerate(loader):
-            if i < 68000:
-                continue
+            image = image[0].permute(1,2,0)
+            image = transform((image * 255).to(torch.uint8))
             path_name = os.path.join(split_dir, str(i) + ".png")
             labels_df.loc[i] = [str(i) + ".png", label]
-            Image.fromarray(image[0].numpy()).save(path_name)
+            Image.fromarray(image).save(path_name)
         labels_df.to_csv(os.path.join(split_dir, "labels.csv"), index=False)
     
 
 if __name__ == "__main__":
 
-    preprocess_all_stl10("/srv/share4/jbjorner3/datasets", "/srv/share4/jbjorner3/Edge_images/Dexi", transforms=DexiAug())
+    # preprocess_all_stl10("/srv/share4/jbjorner3/datasets", "/srv/share4/jbjorner3/Edge_images/Dexi", transform=DexinedAug())
+    preprocess_all_stl10("./datasets", "./Edge_images/Dexi", transform=DexinedAug())
+    # preprocess_all_stl10("./datasets", "./Edge_images/Canny", transform=CannyAug()) This is not useful to do because it has hyper parameters and doesn't take time to run.
+
