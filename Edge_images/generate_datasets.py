6968525b96dd65c72abe4a7593fee5c5d894a64f
jakob-bjorner <jakob_bjorner@hotmail.com>
2022-11-13 18:51:55 -0500

    change dataset interfaces

--- Edge_images/generate_datasets.py
+++ Edge_images/generate_datasets.py
@@ -1,3 +1,15 @@
+"""
+file for generating and using datasets for edge images.
+example usage:
+    from Edge_images.generate_datasets import EdgeDataset
+    ds = DualDataset(CannyDataset("./datasets"), STL10("./datasets", split="train"))
+    for image_canny, image, label in ds:
+        ...
+    # example for dexined edge images
+    ds = DualDataset(DexiNedTrainingDataset, STL10("./datasets", split="train"))
+    for image_dexi, image, label in ds:
+        ...
+"""
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
@@ -7,7 +19,7 @@ from torch.utils.data import Dataset
 from PIL import Image
 from torchvision import datasets
 from data_aug.dexined_aug import CannyAug, DexinedAug
-
+from torchvision import transforms
 class DexiNedDataset(Dataset):
     def __init__(self, 
                 csv_file="datasets/Edge_images/Dexi/train/labels.csv", 
@@ -23,7 +35,7 @@ class DexiNedDataset(Dataset):
     def __getitem__(self, index):
         img_path = os.path.join(self.root_dir, self.labels.iloc[index, 0])
         image = Image.open(img_path)
-        y_label = torch.tensor(int(self.labels.iloc[index, 1]))
+        y_label = eval("torch." + self.labels.iloc[index, 1])
 
         if self.transform:
             image = self.transform(image)
@@ -52,6 +64,28 @@ class DexiNedTestDataset(DexiNedDataset):
                  transform=None):
         super().__init__(csv_file, root_dir, transform)
 
+class CannyDataset(datasets.STL10):
+    """ Class to load the canny edge images from the stl10 dataset. done on the fly as the tranform is very fast. """
+    def __init__(self, root, transform=None, download=True, **kwargs):
+        transform = transforms.Compose([CannyAug()] + (transform if transform in locals() else []))
+        super().__init__(root, transform=transform, download=download, **kwargs)
+class DualDataset(Dataset):
+    """ class for loading two datasets at once. For example DexiNed and original images"""
+    def __init__(self, dataset1: Dataset, dataset2: Dataset):
+        self.dataset1 = dataset1
+        self.dataset2 = dataset2
+        assert len(self.dataset1) == len(self.dataset2)
+    def __len__(self):
+        return len(self.dataset1)
+    def __getitem__(self, index):
+        label1 = self.dataset1[index][1]
+        label2 = self.dataset2[index][1]
+        assert label1 == label2
+        return (self.dataset1[index][0], self.dataset2[index][0], label1)
+class STL10(datasets.STL10):
+    """ Class to load stl10 dataset, just to have everything in one place"""
+    def __init__(self, root, download=True, **kwargs):
+        super().__init__(root, download=download, **kwargs)
 
 def preprocess_all_stl10(stl_data_path, destination_path_root, transform=DexinedAug()):
     """Convert all images from stl10 to edge versions and save them in the same directory"""
@@ -75,11 +109,24 @@ def preprocess_all_stl10(stl_data_path, destination_path_root, transform=Dexined
             labels_df.loc[i] = [str(i) + ".png", label]
             Image.fromarray(image).save(path_name)
         labels_df.to_csv(os.path.join(split_dir, "labels.csv"), index=False)
-    
+
 
 if __name__ == "__main__":
+    ds = CannyDataset("./datasets")
+    ds1 = DualDataset(ds, STL10("./datasets", split="train"))
+    ds2 = DualDataset(DexiNedTrainDataset(), STL10("./datasets", split="train"))
+    import matplotlib.pyplot as plt
+    for ds in [ds1, ds2]:
+        for image_canny, image, label in ds:
+            # display image, and image_canny on a single row
+            fig, (ax1, ax2) = plt.subplots(1, 2)
+            ax1.imshow(image_canny)
+            ax2.imshow(image)
+            print(label)
+            plt.show()
+        
+            print(image_canny, image, label)
+            break
+    
 
-    # preprocess_all_stl10("/srv/share4/jbjorner3/datasets", "/srv/share4/jbjorner3/Edge_images/Dexi", transform=DexinedAug())
-    preprocess_all_stl10("./datasets", "./Edge_images/Dexi", transform=DexinedAug())
-    # preprocess_all_stl10("./datasets", "./Edge_images/Canny", transform=CannyAug()) This is not useful to do because it has hyper parameters and doesn't take time to run.
-
+    
